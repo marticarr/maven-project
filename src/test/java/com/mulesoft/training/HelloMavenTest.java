d2df3a3037c777cc592b277d041411a647c26ad9
Marti Carr <mcarr@acumensolutions.com>
2019-01-15 16:04:13 -0500

    dynamic port

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -2,12 +2,18 @@ package com.mulesoft.training;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
+	@BeforeClass
+	public static void setupPort() {
+		System.setProperty("http.port", "18031");
+	}
+	
 	@Test
 	public void mavenFlowReturnsHelloMaven() throws Exception {
 		runFlowAndExpect("mavenFlow", "Hello Maven");
